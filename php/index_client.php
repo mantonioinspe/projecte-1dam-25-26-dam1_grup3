c8c19555bf77af3161810f676c0c6fc944a3443a
mantonioinspe <a23marmargue@inspedralbes.cat>
2026-05-04 14:22:59 +0200

    Crear i modificacio de botons de index per clients

--- php/index_client.php
+++ php/index_client.php
@@ -1,6 +1,19 @@
-<div class = "encabezado">
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu cliente</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/estils.css">
+</head>
+<body>
+    <div class = "encabezado">
         <h1 class="text-center">GI3P</h1>
         <h1 class="text-center">Institut Pedralbes</h1>
         <p class="text-center">Tens un problema? Digueu-nos</p>
-        <p>Les variables s'han d'utilitzar per a definir la cadena de connexió independentment del codi</p>
     </div>
+    <button><a href="">Crear incidencia</a></button>
+    <button><a href="">Consultar estat de la incidencia</a></button>
+</body>
+</html>
